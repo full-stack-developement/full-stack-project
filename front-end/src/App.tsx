da87619c9e888e47bc8e877a70cbe97bbd4d821f
Ciro Cativo <cirocativo@gmail.com>
2023-02-15 18:57:31 -0400

    feat: InputText Component implemented

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,47 +1,7 @@
-import {
-  Button,
-  ButtonGroup,
-  Stack,
-  useDisclosure,
-  useToast,
-} from "@chakra-ui/react";
-import { ModalExample } from "./components/Modal";
+import { InputText } from "./components/InputText";
 
 function App() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const toast = useToast();
-  function showToast() {
-    toast({
-      title: "Você clicou no Toast.",
-      description: "Sim, você clicou.",
-      status: "success",
-      duration: 2000,
-      isClosable: true,
-    });
-  }
-  return (
-    <>
-      <ModalExample isOpen={isOpen} onClose={onClose} />
-      <Stack direction={"row"} align={"center"} spacing={"4"}>
-        <Button
-          colorScheme={"purple"}
-          margin={4}
-          variant={"solid"}
-          onClick={onOpen}
-        >
-          Abrir modal
-        </Button>
-        <Button
-          colorScheme={"purple"}
-          margin={4}
-          variant={"outline"}
-          onClick={showToast}
-        >
-          Toast
-        </Button>
-      </Stack>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
