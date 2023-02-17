f078f68f18bb6d2d3858e00a08edd74be8caeb73
Katya <katyakeila@gmail.com>
2023-02-17 15:56:52 -0300

    refactor: footer for chakra

--- front-end/src/components/Footer/index.tsx
+++ front-end/src/components/Footer/index.tsx
@@ -1,14 +1,32 @@
-import { Footer1 } from "./styles";
+import { chakra } from "@chakra-ui/react";
 import logoMotorsShop from "../../assets/logoMotorsShop.png";
 
-const Footer = () => {
+export const Footer = () => {
   return (
-    <Footer1>
-      <img src={logoMotorsShop} alt="logo" />
-      <p>&copy; 2002 - Todos os direitos reservados</p>
-      <button>^</button>
-    </Footer1>
+    <chakra.div
+      display="flex"
+      flexDirection="row"
+      justifyContent="space-between"
+      alignItems="center"
+      padding="45px 59px"
+      gap="10px"
+      backgroundColor="greyScale.grey0"
+      color="greyScale.whiteFixed"
+      fontFamily="Inter"
+      fontSize='font["body-2-400"].size'
+      fontWeight='font["body-2-400"].weigth'
+    >
+      <chakra.img src={logoMotorsShop} alt="logo" />
+      <chakra.p>&copy; 2002 - Todos os direitos reservados</chakra.p>
+      <chakra.button
+        backgroundColor="greyScale.grey1"
+        color="greyScale.whiteFixed"
+        width="53px"
+        height="50px"
+        borderRadius="4px"
+      >
+        ^
+      </chakra.button>
+    </chakra.div>
   );
 };
-
-export default Footer;
