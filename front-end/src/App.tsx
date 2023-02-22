fddb224d084adefb08d6ad5217416bbfeaa4e82c
artorys <darksoulsgui123@gmail.com>
2023-02-22 16:51:44 -0300

    fix: return app <>

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,8 +1,6 @@
-import { ModalAnnouncement } from "./components/ModalAnnouncement";
-
 function App() {
   return (
-    <ModalAnnouncement></ModalAnnouncement>
+    <></>
   );
 }
 export default App;
