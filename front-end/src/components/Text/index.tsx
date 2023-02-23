f978fe106ced0cf4c483f8ca6147488aa7cc403f
artorys <darksoulsgui123@gmail.com>
2023-02-23 10:06:14 -0300

    fix: type variants in button component

--- front-end/src/components/Text/index.tsx
+++ front-end/src/components/Text/index.tsx
@@ -1,6 +1,6 @@
 import {Text as TextChakra} from "@chakra-ui/react"
 
-type typeVariants = "title-content-form" | "label-content-form" | "errors-form" // tipe suas variantes aqui
+type typeVariants = "title-content-form" | "label-content-form" | "errors-form" | "text-delete" // tipe suas variantes aqui
 
 interface ITextProps{
     text: string
