35ed9c5e56647b0df99dac34616ac92e3bab7761
artorys <darksoulsgui123@gmail.com>
2023-02-23 13:11:48 -0300

    fix: app empty

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,8 +1,6 @@
-import { ModalAnnouncementDelete } from "./components/ModalAnnouncementDelete";
-
 function App() {
   return (
-    <><ModalAnnouncementDelete></ModalAnnouncementDelete></>
+    <></>
   );
 }
 export default App;
