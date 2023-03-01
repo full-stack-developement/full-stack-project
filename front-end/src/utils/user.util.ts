36194bdca29c39cc3828deed8ef7e9b03511f86c
artorys <darksoulsgui123@gmail.com>
2023-03-01 10:53:13 -0300

    feat: add utils for user

--- front-end/src/utils/user.util.ts
+++ front-end/src/utils/user.util.ts
@@ -1,8 +1,38 @@
 import { AxiosError } from "axios";
 import { FieldValues } from "react-hook-form";
-import { apiAnnouncement, apiUser } from "../api";
+import { apiUser } from "../api";
 import { IUserUpdate } from "../interfaces/user.interface";
 
+export async function createUser(data : FieldValues){
+    let responseCreate = {data : {} as IUserUpdate,message : "" as "success" | "error"}
+    try{
+        const response = await apiUser.post("",data)
+        responseCreate.data = response.data
+        responseCreate.message = "success"
+        return responseCreate
+    }
+    catch(err){
+        if(err instanceof AxiosError){
+            responseCreate.message = "error"
+            return responseCreate
+        }
+    }
+}
+export async function loginUser(data : FieldValues){
+    let responseLogin = {data : {} as IUserUpdate,message : "" as "success" | "error"}
+    try{
+        const response = await apiUser.post("/login",data)
+        responseLogin.data = response.data
+        responseLogin.message = "success"
+        return responseLogin
+    }
+    catch(err){
+        if(err instanceof AxiosError){
+            responseLogin.message = "error"
+            return responseLogin
+        }
+    }
+}
 
 export async function updateUser(data : FieldValues,id : string){
     let responseUpdate = {data : {} as IUserUpdate,message : "" as "success" | "error"}
