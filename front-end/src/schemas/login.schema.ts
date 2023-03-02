ecbb354ad538b28f019bc23748dff7fe076bceee
artorys <darksoulsgui123@gmail.com>
2023-03-02 15:46:07 -0300

    fix: change username to email

--- front-end/src/schemas/login.schema.ts
+++ front-end/src/schemas/login.schema.ts
@@ -1,6 +1,6 @@
 import * as yup from "yup"
 
 export const loginSchema = yup.object().shape({
-    username : yup.string().required("O username é obrigatório"),
+    email : yup.string().required("O email é obrigatório"),
     password : yup.string().required("A senha é obrigatória")
 })
