a5f44c176944b598323ee1cd871d4f09c68c66c2
João Maia <joaocarlosbgm@gmail.com>
2023-03-02 16:08:35 -0300

    fix: form data axios receive

--- front-end/src/interfaces/user.interface.ts
+++ front-end/src/interfaces/user.interface.ts
@@ -8,3 +8,22 @@ export interface IUserCreate {
   description: string;
   password: string;
 }
+export interface IUserUpdate {
+  id: string;
+  full_name: string;
+  email: string;
+  cpf: number;
+  phone: number;
+  birthDate: Date;
+  description: string;
+  password: string;
+  address: IAddress;
+}
+interface IAddress {
+  cep: number;
+  state: string;
+  city: string;
+  street: string;
+  number: number;
+  complement: string;
+}
--- front-end/src/utils/user.util.ts
+++ front-end/src/utils/user.util.ts
@@ -1,6 +1,7 @@
 import { AxiosError } from "axios";
 import { FieldValues } from "react-hook-form";
 import { apiUser } from "../api";
+import { IUserUpdate } from "../interfaces/user.interface";
 
 export async function createUser(data: FieldValues) {
   const addressData = {
@@ -26,7 +27,6 @@ export async function createUser(data: FieldValues) {
       }
     });
 }
-import { IUserUpdate } from "../interfaces/user.interface";
 
 export async function loginUser(data: FieldValues) {
   let responseLogin = { data: "", message: "" as "success" | "error" };
@@ -39,30 +39,6 @@ export async function loginUser(data: FieldValues) {
     if (err instanceof AxiosError) {
       responseLogin.message = "error";
       return responseLogin;
-    }
-  }
-}
-
-export async function updateUser(data: FieldValues, id: string) {
-  let responseUpdate = {
-    data: {} as IUserUpdate,
-    message: "" as "success" | "error",
-  };
-  try {
-    let validatedData = {} as FieldValues;
-    for (const key in data) {
-      if (data[key]) {
-        validatedData[key] = data[key];
-      }
-    }
-    const response = await apiUser.patch(`/${id}`, validatedData);
-    responseUpdate.data = response.data;
-    responseUpdate.message = "success";
-    return responseUpdate;
-  } catch (err) {
-    if (err instanceof AxiosError) {
-      responseUpdate.message = "error";
-      return responseUpdate;
     }
   }
 }
