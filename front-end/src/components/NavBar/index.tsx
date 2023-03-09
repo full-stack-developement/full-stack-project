c3cc91d39f6751881d3fa99ed96e703ce8db3e77
artorys <darksoulsgui123@gmail.com>
2023-03-08 21:34:45 -0300

    feat: add feat/hamburguer_menu

--- front-end/src/components/NavBar/index.tsx
+++ front-end/src/components/NavBar/index.tsx
@@ -1,4 +1,4 @@
-import { chakra } from "@chakra-ui/react";
+import { chakra, useMediaQuery } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import logo from "../../assets/logo.svg";
 import { customTheme } from "../../theme";
@@ -7,6 +7,7 @@ import { NavBarButtons } from "../NavBarButtons";
 
 export const NavBar = () => {
   const navigate = useNavigate()
+  const [lessThan1200] = useMediaQuery("(min-width: 1200px)")
 
   return (
     <chakra.div
@@ -16,17 +17,17 @@ export const NavBar = () => {
       justifyContent="space-between"
       alignItems="center"
       height="80px"
-      px="60px"
+      px="0px"
       borderBottom="2px solid"
       borderBottomColor={customTheme.colors.greyScale.grey6}
     >
-      <chakra.img _hover={{cursor : "pointer"}} onClick={()=>{
+      <chakra.img ml={"1rem"} _hover={{cursor : "pointer"}} onClick={()=>{
         navigate("/home")
       }} src={logo} alt="logo" height="26px" />
       <chakra.div height="full" display="flex" alignItems="center">
-        <NavBarButtons />
+        {lessThan1200 && <NavBarButtons />}
         <LoginBar />
-      </chakra.div>
+      </chakra.div> 
     </chakra.div>
   );
 };
